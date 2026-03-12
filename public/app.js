619f436a1d88e10de0cef3b5374615dca4a9e06a
ZikannSenpai <zidanriski778@gmail.com>
2026-03-13 03:10:43 +0700

    +

--- public/app.js
+++ public/app.js
@@ -1,3 +1,13 @@
+/* Window */
+window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    const percent = (scrollTop / height) * 100;
+    document.getElementById("scroll-bar").style.width = percent + "%";
+});
+
 /* Start Func*/
 function time() {
     const time = document.getElementById("time");
