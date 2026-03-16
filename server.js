38f240215585cb99f9be343e0147dbeaee705b2d
ZikaNyawDev <zidanriski778@gmail.com>
2026-03-16 22:34:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/searchanime", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/project/index.html"));
+    res.sendFile(path.join(__dirname, "public/project/main.html"));
 });
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
