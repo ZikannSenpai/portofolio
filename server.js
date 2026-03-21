8a03f047526f6aef3cfd90a8b7c0789a3512ba25
ZikannSenpai <zidanriski778@gmail.com>
2026-03-21 21:20:36 +0700

    +

--- server.js
+++ server.js
@@ -15,12 +15,19 @@ app.use("/project", express.static(path.join(__dirname, "public/project")));
 app.get("/donate", (req, res) => {
     res.sendFile(path.join(__dirname, "public/project/donasi/donasi.html"));
 });
+
+app.get("/spotify", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/project/spotify/spotify.html"));
+});
+
 app.get("/downloadanime", (req, res) => {
     res.sendFile(path.join(__dirname, "public/project/batch/main.html"));
 });
+
 app.get("/searchanime", (req, res) => {
     res.sendFile(path.join(__dirname, "public/project/anime Search/main.html"));
 });
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
