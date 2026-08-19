26fae5945a22fc9286cdad8eb030dd1dd5ddbc62
ZikannSenpai <zidanriski778@gmail.com>
2026-08-19 17:54:27 +0700

    +

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -1,6 +1,6 @@
 import {
     SiHtml5,
-    SiCss3,
+    SiCss,
     SiJavascript,
     SiTypescript,
     SiReact,
@@ -14,18 +14,54 @@ import {
 } from "react-icons/si";
 
 const tech = [
-    { name: "HTML5", icon: <SiHtml5 /> },
-    { name: "CSS3", icon: <SiCss3 /> },
-    { name: "JavaScript", icon: <SiJavascript /> },
-    { name: "TypeScript", icon: <SiTypescript /> },
-    { name: "React", icon: <SiReact /> },
-    { name: "Next.js", icon: <SiNextdotjs /> },
-    { name: "Tailwind CSS", icon: <SiTailwindcss /> },
-    { name: "Node.js", icon: <SiNodedotjs /> },
-    { name: "Express", icon: <SiExpress /> },
-    { name: "Git", icon: <SiGit /> },
-    { name: "GitHub", icon: <SiGithub /> },
-    { name: "Figma", icon: <SiFigma /> }
+    {
+        name: "HTML5",
+        icon: <SiHtml5 />
+    },
+    {
+        name: "CSS3",
+        icon: <SiCss />
+    },
+    {
+        name: "JavaScript",
+        icon: <SiJavascript />
+    },
+    {
+        name: "TypeScript",
+        icon: <SiTypescript />
+    },
+    {
+        name: "React",
+        icon: <SiReact />
+    },
+    {
+        name: "Next.js",
+        icon: <SiNextdotjs />
+    },
+    {
+        name: "Tailwind CSS",
+        icon: <SiTailwindcss />
+    },
+    {
+        name: "Node.js",
+        icon: <SiNodedotjs />
+    },
+    {
+        name: "Express",
+        icon: <SiExpress />
+    },
+    {
+        name: "Git",
+        icon: <SiGit />
+    },
+    {
+        name: "GitHub",
+        icon: <SiGithub />
+    },
+    {
+        name: "Figma",
+        icon: <SiFigma />
+    }
 ];
 
 export default function TechStack() {
